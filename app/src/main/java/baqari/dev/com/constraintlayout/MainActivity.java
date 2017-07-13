7460050af33c08a7837248976da3411ff86cc1aa
pirati02 <baqari20@gmail.com>
2017-07-13 15:16:10 +0400

    circle.yml

--- app/src/main/java/baqari/dev/com/constraintlayout/MainActivity.java
+++ app/src/main/java/baqari/dev/com/constraintlayout/MainActivity.java
@@ -2,7 +2,7 @@ package baqari.dev.com.constraintlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
- 
+
 
 public class MainActivity extends AppCompatActivity {
 

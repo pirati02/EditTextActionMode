d045f574178d5c1b95087dccb0f218d58933c20a
pirati02 <baqari20@gmail.com>
2017-07-13 15:05:49 +0400

    Initial commit

---
+++ app/src/main/java/baqari/dev/com/constraintlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package baqari.dev.com.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+ 
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String name = null;
+        String error = name.substring(0, 3);
+    }
+}
